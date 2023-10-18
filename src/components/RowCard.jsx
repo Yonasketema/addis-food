aa04bae1192707b8797e0c1e94d1042d205db93b
yonasketema <yonask.kitaw@gmail.com>
2023-10-18 22:56:23 +0300

    add Row Components

---
+++ src/components/RowCard.jsx
@@ -0,0 +1,47 @@
+import React from "react";
+import styled from "styled-components";
+import Row from "./Row";
+import ButtonGroup from "./ButtonGroup";
+import Button from "./Button";
+
+const Image = styled.img`
+  width: auto;
+  height: inherit;
+`;
+
+const StyledRowCard = styled.div`
+  display: flex;
+  gap: 0.8rem;
+  height: 14rem;
+  width: 75%;
+  border: 1px solid #ddd;
+`;
+
+const Container = styled.div`
+  width: 100%;
+  padding: 0.8rem;
+`;
+
+function RowCard() {
+  return (
+    <StyledRowCard>
+      <Image src="img-6.jpg" />
+      <Container>
+        <div>
+          <h1>Shiro</h1>
+          <p>Shiro, karia, timatim</p>
+        </div>
+        <Row>
+          <span>$200.0</span>
+
+          <ButtonGroup>
+            <Button>Edit</Button>
+            <Button variation="danger">Delete</Button>
+          </ButtonGroup>
+        </Row>
+      </Container>
+    </StyledRowCard>
+  );
+}
+
+export default RowCard;
