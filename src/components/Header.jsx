68a8dfafc07f72356699c4b6d527fe0c87bf237c
yonasketema <yonask.kitaw@gmail.com>
2023-10-26 19:17:22 +0300

    add Logo

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,4 +1,5 @@
 import styled from "styled-components";
+import Logo from "./Logo";
 
 const StyledHeader = styled.header`
   background-color: var(--color-grey-0);
@@ -7,7 +8,11 @@ const StyledHeader = styled.header`
 `;
 
 function Header() {
-  return <StyledHeader>HEADER</StyledHeader>;
+  return (
+    <StyledHeader>
+      <Logo />
+    </StyledHeader>
+  );
 }
 
 export default Header;
