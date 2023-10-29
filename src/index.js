b2842aa50742ca57d4ed71904d74122ca22d27d9
yonasketema <yonask.kitaw@gmail.com>
2023-10-29 23:12:22 +0300

    add toaster

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+import { Toaster } from "react-hot-toast";
 
 import "./index.css";
 import App from "./App";
@@ -23,5 +24,26 @@ root.render(
       <App />
       <ReactQueryDevtools initialIsOpen={false} />
     </QueryClientProvider>
+
+    <Toaster
+      position="top-right"
+      gutter={12}
+      containerStyle={{ margin: "7px" }}
+      toastOptions={{
+        success: {
+          duration: 3000,
+        },
+        error: {
+          duration: 5000,
+        },
+        style: {
+          fontSize: "12px",
+          maxWidth: "500px",
+          padding: "16px 24px",
+          backgroundColor: "var(--color-grey-0)",
+          color: "var(--color-grey-700)",
+        },
+      }}
+    />
   </React.StrictMode>
 );
