0dca287d3a339d80cc734d400a0ab9962aecd829
yonasketema <yonask.kitaw@gmail.com>
2023-12-25 21:50:00 +0300

    fix: navigate path

--- src/components/SignupForm.jsx
+++ src/components/SignupForm.jsx
@@ -1,12 +1,12 @@
 import { useForm } from "react-hook-form";
+import { useNavigate } from "react-router-dom";
+import { useSignup } from "../hooks/useSignup";
+
+import SpinnerMini from "./SpinnerMini";
 import Form from "./Form";
 import FormRow from "./FormRow";
 import Input from "./Input";
 import Button from "./Button";
-
-import { useNavigate } from "react-router-dom";
-import { useSignup } from "../hooks/useSignup";
-import SpinnerMini from "./SpinnerMini";
 
 // Email regex: /\S+@\S+\.\S+/
 
@@ -25,7 +25,7 @@ function SignupForm() {
           reset();
         },
         onSuccess: (user) => {
-          navigate("/dashboard");
+          navigate("/");
         },
       }
     );
