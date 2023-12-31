f39de7841b45e07f33dce2c18a5fb4f01e82e4c7
yonasketema <yonask.kitaw@gmail.com>
2023-12-31 14:29:30 +0300

    refactor : priceContainer from `foodCard`

---
+++ src/components/PriceContainer.jsx
@@ -0,0 +1,44 @@
+import React from "react";
+import styled from "styled-components";
+import Heading from "./Heading";
+
+const StyledPriceContainer = styled.div`
+  display: flex;
+  align-items: center;
+  gap: 0.7rem;
+`;
+
+const DiscountPriceBox = styled.div`
+  display: flex;
+  align-items: baseline;
+  text-decoration: line-through;
+  font-weight: bold;
+  color: var(--color-grey-600);
+`;
+
+const PriceBox = styled.div`
+  display: flex;
+  align-items: baseline;
+  font-weight: bold;
+  color: var(--color-grey-900);
+`;
+
+function PriceContainer({ discountPrice, price }) {
+  return (
+    <StyledPriceContainer>
+      {discountPrice && (
+        <DiscountPriceBox>
+          <Heading as="h2">{price}</Heading>
+          <small>Birr</small>
+        </DiscountPriceBox>
+      )}
+
+      <PriceBox>
+        <Heading as="h2">{discountPrice || price}</Heading>
+        <small>Birr</small>
+      </PriceBox>
+    </StyledPriceContainer>
+  );
+}
+
+export default PriceContainer;
