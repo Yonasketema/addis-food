c4a69cb0bfba9734e34267bd46c5970c5c52f544
yonasketema <yonask.kitaw@gmail.com>
2024-02-10 20:42:25 +0300

    refactor: with styled-components

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,86 +1,106 @@
-import styled from "styled-components";
-import { Link, NavLink } from "react-router-dom";
+import styled, { css } from "styled-components";
+import { Link, NavLink, useLocation } from "react-router-dom";
 
-import Row from "./ui/Row";
 import LogoutButton from "./LogoutButton";
-import { useUser } from "../hooks/useUser";
 
 const StyledHeader = styled.header`
   background-color: var(--color-grey-0);
-  padding: 1.2rem 4.8rem;
+  padding: 3rem;
   border-bottom: 1px solid var(--color-grey-100);
   height: 13vh;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
 `;
 
-const StyledLink = styled(NavLink)`
-  &:link,
-  &:visited {
-    text-transform: uppercase;
-    color: var(--color-grey-500);
-    font-size: 1.6rem;
-    font-weight: 600;
-    padding: 1.2rem 2.4rem;
-    transition: all 0.3s;
+const Nav = styled.nav`
+  display: flex;
+  align-items: center;
+  gap: 3.5rem;
+`;
+
+const StyledLink = styled(Link)`
+  color: yellowgreen;
+  font-weight: bold;
+  margin-left: 0.5rem;
+
+  &:hover {
+    text-decoration: underline;
   }
+`;
+
+const StyledNavLink = styled(NavLink)`
+  text-transform: uppercase;
+  color: var(--color-grey-500);
+  font-size: 1.6rem;
+  font-weight: 600;
+  transition: all 0.3s;
 
   &:hover,
   &:active,
   &.active:link,
-  &.active:visited {
+  &.active {
     color: yellowgreen;
   }
+
+  ${(props) =>
+    props.btn &&
+    css`
+      color: #fff;
+      padding: 1.2rem 2.4rem;
+      background-color: yellowgreen;
+      box-shadow: var(--shadow-md);
+      border-radius: 5px;
+      text-transform: capitalize;
+
+      &:hover,
+      &:active,
+      &.active:link,
+      &.active:visited {
+        color: white;
+      }
+    `}
 `;
 
 function Header({ user }) {
+  const location = useLocation();
+  const currentUrl = location.pathname;
   return (
     <StyledHeader>
-      <Row>
-        <Link to="/">
-          <h3>Addis Foods </h3>
-        </Link>
+      <Link to="/">
+        <h3>Addis Foods </h3>
+      </Link>
 
-        {user ? (
-          <header
-            style={{
-              display: "flex",
-              gap: "1rem",
-              alignItems: "center",
-            }}
-          >
-            {user.role === "admin" ? (
-              <>
-                <StyledLink to="/dashboard">My Menu</StyledLink>
-              </>
-            ) : (
-              <StyledLink to="/create">Create Menu</StyledLink>
-            )}
+      {user ? (
+        <Nav>
+          {user.role === "admin" ? (
+            <>
+              <StyledNavLink to="/dashboard">My Menu</StyledNavLink>
+            </>
+          ) : (
+            <StyledNavLink to="/create">Create Menu</StyledNavLink>
+          )}
 
-            {user.role === "user" && <LogoutButton />}
-          </header>
-        ) : (
-          <header
-            style={{
-              display: "flex",
-              alignItems: "center",
-              gap: "1rem",
-            }}
-          >
-            <StyledLink to="/how-it-work">How it Work</StyledLink>
-            <StyledLink to="/signup">Get started</StyledLink>
-            <StyledLink to="/login">Login</StyledLink>
+          {user.role === "user" && <LogoutButton />}
+        </Nav>
+      ) : (
+        <Nav>
+          {currentUrl === "/signup" ? (
             <p>
-              Already have an account?{" "}
-              <u
-                style={{
-                  color: "greenyellow",
-                }}
-              >
-                Login
-              </u>
+              Already have an account?
+              <StyledLink to="/login">Login</StyledLink>
             </p>
-          </header>
-        )}
-      </Row>
+          ) : (
+            <>
+              <StyledNavLink to="/how-it-work">How it Work</StyledNavLink>
+              <StyledNavLink to="/login">Log in</StyledNavLink>
+              <StyledNavLink to="/signup" btn>
+                Sign up
+              </StyledNavLink>
+            </>
+          )}
+        </Nav>
+      )}
     </StyledHeader>
   );
 }
